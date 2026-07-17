b02ee3685a801a8c4c5ea07e3ef70371fa5fbca4
Tarunna Sen <tarunnasen@gmail.com>
2026-07-18 01:02:46 +0600

    Manage Wallboards page

---
+++ src/components/ManageWallboard.tsx
@@ -0,0 +1,49 @@
+import Image from "next/image";
+import Button from "./Button";
+
+interface ManageWallboardProps {
+    name: string;
+    code: string;
+    description: string;
+    image: string;
+    id: string;
+}
+
+export default function ManageWallboard(props: ManageWallboardProps) {
+    return (
+      <div className="flex gap-4 items-center justify-between hover:scale-105 transition-transform duration-300 py-4 px-8 border border-gray-300">
+        <div className="flex flex-row gap-4 items-center">
+          <div>
+            <Image
+              height={100}
+              width={100}
+              src={props.image}
+              alt={props.name}
+              className="rounded-lg"
+            />
+          </div>
+          <div className="flex flex-col">
+            <h3 className="font-bold text-2xl">
+              {props.name}
+            </h3>
+            <p className="text-sm">
+              {props.code}
+            </p>
+            <p className="my-4 text-justify">{props.description}</p>
+          </div>
+        </div>
+        <div className="flex gap-4 justify-self-end">
+          <Button
+            text="Edit"
+            type="secondary"
+            task="button"
+          />
+          <Button
+            text="Delete"
+            type="risk"
+            task="button"
+          />
+        </div>
+      </div>
+    );
+}

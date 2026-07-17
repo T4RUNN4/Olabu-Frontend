7c07e5815f53145097ba31fa02b61f7ec7e887b1
Tarunna Sen <tarunnasen@gmail.com>
2026-07-17 20:06:17 +0600

    First Setup

---
+++ src/app/layout.tsx
@@ -0,0 +1,33 @@
+import type { Metadata } from "next";
+import { Poppins } from "next/font/google";
+import "./globals.css";
+import Navbar from "@/components/Navbar";
+
+const poppins = Poppins({
+  subsets: ["latin"],
+  weight: ["400", "500", "600", "700"],
+});
+
+export const metadata: Metadata = {
+  title: "OLABU",
+  description: "OLABU is a bangladeshi customized PVC wallboard business with the best quality possible",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html
+      lang="en"
+      data-theme="light"
+      className={`${poppins.className} h-full antialiased`}
+    >
+      <body className="min-h-full flex flex-col px-10 md:px-30">
+        <Navbar />
+        {children}
+      </body>
+    </html>
+  );
+}

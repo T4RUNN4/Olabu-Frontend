ce3f5527532d89093c24e0f900f04b4c36845855
Tarunna Sen <tarunnasen@gmail.com>
2026-07-18 01:48:31 +0600

    React-hook-form integration in add wallboards

--- src/app/add-wallboards/page.tsx
+++ src/app/add-wallboards/page.tsx
@@ -1,21 +1,44 @@
+"use form"
+
 import Button from "@/components/Button";
+import FormErrorMessage from "@/components/FormErrorMessage";
 import FormLabel from "@/components/FormLabel";
 import SectionWrapper from "@/components/SectionWrapper";
+import { useForm, SubmitHandler } from "react-hook-form";
+
+type Inputs = {
+  name: string;
+  code: string;
+  image: string;
+  description: string;
+};
 
 export default function AddWallboards() {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<Inputs>();
+  const onSubmit: SubmitHandler<Inputs> = (data) => console.log(data);
+
   return (
     <SectionWrapper
       heading="Add Wallboards"
       subheading="Add wallboards to your collection"
     >
-      <form className="fieldset border-base-300 rounded-box border p-12">
+      <form
+        onSubmit={handleSubmit(onSubmit)}
+        className="fieldset border-base-300 rounded-box border p-12"
+      >
         <div className="flex flex-col gap-2 justify-center mb-4">
           <FormLabel text="Wallboard Name" isRequired />
           <input
             type="text"
             className="input w-full focus:border focus:border-gray-500"
             placeholder="Everything in time"
+            {...register("name", { required: "Wallboard name is Required" })}
           />
+          {errors.name && <FormErrorMessage text={errors.name.message} />}
         </div>
 
         <div className="flex flex-col gap-2 justify-center mb-4">
@@ -24,7 +47,9 @@ export default function AddWallboards() {
             type="text"
             className="input w-full focus:border focus:border-gray-500"
             placeholder="lm10-wc-01"
+            {...register("code", { required: "Wallboard code is Required" })}
           />
+          {errors.code && <FormErrorMessage text={errors.code.message} />}
         </div>
 
         <div className="flex flex-col gap-2 justify-center mb-4">
@@ -33,7 +58,9 @@ export default function AddWallboards() {
             type="text"
             className="input w-full focus:border focus:border-gray-500"
             placeholder="https://example.com"
+            {...register("image", { required: "Wallboard image is Required" })}
           />
+          {errors.image && <FormErrorMessage text={errors.image.message} />}
         </div>
 
         <div className="flex flex-col gap-2 justify-center mb-4">
@@ -42,10 +69,21 @@ export default function AddWallboards() {
             type="text"
             className="input w-full focus:border focus:border-gray-500"
             placeholder="The wallboards features the iconic moment of Lionel Messi receiving Fifa World Cup 2022."
+            {...register("description", {
+              required: "Wallboard description is Required",
+            })}
           />
+          {errors.description && (
+            <FormErrorMessage text={errors.description.message} />
+          )}
         </div>
 
-        <Button text="Add to the Collection" type="primary" task="button" btnType="submit" />
+        <Button
+          text="Add to the Collection"
+          type="primary"
+          task="button"
+          btnType="submit"
+        />
       </form>
     </SectionWrapper>
   );

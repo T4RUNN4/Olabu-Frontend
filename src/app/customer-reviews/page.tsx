f5837821488a8dd1d605aa143ae2228ab85252aa
Tarunna Sen <tarunnasen@gmail.com>
2026-07-19 19:25:40 +0600

    Improved componet for future database integration

--- src/app/customer-reviews/page.tsx
+++ src/app/customer-reviews/page.tsx
@@ -5,7 +5,6 @@ export default function CustomerReview() {
   const reviews = [
     {
       name: "Satisfied Customer",
-      gender: "male",
       date: "June 11, 2026",
       review:
         "The image and board quality is good. Gives a premium vibe. Overally 8.8/10",
@@ -13,7 +12,6 @@ export default function CustomerReview() {
     },
     {
       name: "Satisfied Customer",
-      gender: "female",
       date: "June 12, 2026",
       review:
         "Very good quality. Images are really good. I brought some boards previously whch has blurry pictures. But yours are really good in quality.",
@@ -21,12 +19,18 @@ export default function CustomerReview() {
     },
     {
       name: "Satisfied Customer",
-      gender: "female",
       date: "June 12, 2026",
       review:
         "Boards are really good in quality. Colors are really vibrant too.",
       id: "c3"
     },
+  ];
+
+  const proflePicture = [
+    "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQu5uyN1gxdNOtHvg9lzEJv4h6__3vp65KH8rQ103eyeA&s=10",
+    "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSkkkrDlGuxlbDQzF-nzIsy1r0IciQ95V8gzZQwLwkaU15bz9lXUtLAi4ds&s=10",
+    "https://cdn.freepixel.com/thumb/free-PNG-graphics-cheerful-cartoon-coconut-character-on-png-background-th-1004413114.jpg",
+    "https://img.magnific.com/free-vector/kawaii-strawberry-fruits-flat-style_1951-778.jpg?semt=ais_hybrid&w=740&q=80",
   ];
 
   return (
@@ -36,15 +40,23 @@ export default function CustomerReview() {
     >
       <div className="flex flex-col justify-center gap-4 mt-8">
         {reviews.map((review) => (
-            <CustomerCard key={review.id} name={review.name} gender={review.gender} date={review.date} review={review.review} />
+          <CustomerCard
+            key={review.id}
+            name={review.name}
+            image={
+              proflePicture[Math.floor(Math.random() * proflePicture.length)]
+            }
+            date={review.date}
+            review={review.review}
+          />
         ))}
       </div>
 
       <p className="text-xs text-gray-600 mt-20">
-        All of this reviews are collected from customers. We
-        hide customers identity unless they share it publicly. We recieve
-        reviews in Bengali and English both languages. But they are translated
-        here better integration.
+        All of this reviews are collected from customers. We hide customers
+        identity unless they share it publicly. We recieve reviews in Bengali
+        and English both languages. But they are translated here better
+        integration.
       </p>
     </SectionWrapper>
   );
--- src/components/CustomerCard.tsx
+++ src/components/CustomerCard.tsx
@@ -1,32 +1,28 @@
 import Image from "next/image";
 
 interface CustomerCardProps {
-    name: string;
-    gender: string;
-    date: string;
-    review: string;
+  name: string;
+  image: string;
+  date: string;
+  review: string;
 }
 
 export default function CustomerCard(props: CustomerCardProps) {
-    return (
-      <div className="flex flex-col items-center md:flex-row gap-4 p-4 border border-gray-300 hover:scale-105 transition-transform duration-200">
-        <div className="h-20 w-20 rounded-full flex items-center justify-center overflow-hidden border border-gray-200">
-          <Image
-            alt={props.name}
-            src={
-              props.gender === "male"
-                ? "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTojCezaZ85X2c7xvbqX_xXln264upnrThXcJcsAbzOHGS1gmA0FbyetAQn&s=10"
-                : "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSRt1akkUB-VZlSAs6v8ptLIdlOwjACEBQo2Qna5fhJNg&s=10"
-            }
-            height={80}
-            width={80}
-          />
-        </div>
-        <div className="flex flex-col">
-            <h1 className="text-xl font-semibold">{props.name}</h1>
-            <h2 className="text-sm text-gray-500 mb-4">{props.date}</h2>
-            <p>{props.review}</p>
-        </div>
+  return (
+    <div className="flex flex-col items-center md:flex-row gap-4 p-4 border border-gray-300 hover:scale-105 transition-transform duration-200">
+      <div className="h-20 w-20 rounded-full flex items-center justify-center overflow-hidden border border-gray-200">
+        <Image
+          alt={props.name}
+          src={props.image}
+          height={80}
+          width={80}
+        />
       </div>
-    );
+      <div className="flex flex-col">
+        <h1 className="text-xl font-semibold">{props.name}</h1>
+        <h2 className="text-sm text-gray-500 mb-4">{props.date}</h2>
+        <p>{props.review}</p>
+      </div>
+    </div>
+  );
 }
